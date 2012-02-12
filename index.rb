ec406e650b9a7314572515f574af7b5a94869e8d
René Hansen <renehh@gmail.com>
2012-02-12 01:51:06 +0100

    Fixed nil error on empty query.

--- index.rb
+++ index.rb
@@ -7,6 +7,7 @@ get '/' do
   https_session = Net::HTTP.new(uri.host, uri.port)
   https_session.use_ssl = true if uri.port == 443
   https_session.start
-  response = https_session.get(uri.path + "?" + uri.query)
+  # uri.query can be nil and cause an exception
+  response = https_session.get(uri.path + "?" + uri.query.to_s) # to_s in case nil
   response.body
 end
